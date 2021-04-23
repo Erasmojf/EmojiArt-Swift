eaa11015de14fd66c2b86a2b6ad3fc379075372f
Erasmo J.F Da Silva <erasmogohiper@gmail.com>
2021-04-23 10:01:57 -0300

    CM - Commit inicial da criacao do emojiart
    Commit inicial da criacao do emojiart

---
+++ EmojiArts/Shared/EmojiArtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtsApp.swift
+//  Shared
+//
+//  Created by Erasmo J.F Da Silva on 23/04/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
